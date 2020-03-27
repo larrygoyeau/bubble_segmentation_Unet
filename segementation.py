ae6d3bea9be2955249d2b18b1ca413ef6a48b7dd
larrygoyeau <52501684+larrygoyeau@users.noreply.github.com>
2020-03-27 16:34:33 +0100

    Update segementation.py

--- segementation.py
+++ segementation.py
@@ -226,7 +226,7 @@ def segment_image(uploaded):
       augmentation=get_validation_augmentation
     )
 
-    minimum_size_bubble=5
+    minimum_size_bubble=8
 
     image= test_dataset[0]
     image = np.expand_dims(image, axis=0)
